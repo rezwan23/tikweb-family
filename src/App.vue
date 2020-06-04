97042a3d3cba18d9818e81c834dffbc267bc1475
Ghani Razwanul <razwanulghani@gmail.com>
2020-06-04 20:22:43 +0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Hello World!</h1>
+  </div>
+</template>
+
+<script>
+
+export default{
+  data(){
+    return {
+      
+    }
+  },
+  methods : {
+    init(){
+      console.log("Hi!! This is GHANI");
+    }
+  },
+  mounted(){
+    this.init();
+  },
+}
+
+</script>
+
+<style scoped>
+
+</style>
